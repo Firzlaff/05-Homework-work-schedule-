627c831af984c6a6efe031ac2347a053856f1f4d
Firzlaff <tysenfirzlaff@gmail.com>
2020-04-04 19:36:20 -0600

    adding moment and adding current day and itme to top of app

---
+++ develop/script.js
@@ -0,0 +1,38 @@
+// HTML Nodes 
+
+// create time blocks
+//  ->timeblock has three pieces
+     
+//     *text atea f*timor activity
+//         -> collect user information
+//         -> form element? which one?
+//     *save button
+//         -> stores the activity in local storage
+//           -> stores *EACH* activity in local storage
+//           -> how can I make sure iI does'nt overwrite what's already in there?
+
+//         -> store the time in local storage  -- JSONStringify  and JSONParse
+//             * save needs to go to the correct hour/slot
+
+// display time blocks
+
+// color code the activity
+// -> get the current time
+// -> compare time block againts current time -- ??? Moment.js
+//     *condition for past event update class to have BG or RED
+//     *condition for present event update class to have BG or Green
+//     *condition for future event update class to have BG or Light Grey
+
+// ON REFRESH 
+// -> time blocks should populate with local storage data
+//     * retrieve data from local storage
+//     * differentiate times
+//     * display local storage data on screen
+
+//     time | activity | save button
+//     ->When we hit save, 
+//       storing the time and activity in local storage
+
+//       STARTING WITH GETTING THE CODE TO SAVE FOR ONE FIELD FIRST.
+
+//       treat local storage like its its own JS Object 
