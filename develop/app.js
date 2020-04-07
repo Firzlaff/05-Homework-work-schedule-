479b39662b3894557bace576146810aa1dd00b87
Firzlaff <tysenfirzlaff@gmail.com>
2020-04-07 07:21:51 -0600

    still cant get app.js to link anymore, it will not show my first console log and shows i have a syntax error on js ln 17 that I cannot find or fix :(

--- develop/app.js
+++ develop/app.js
@@ -7,42 +7,43 @@ $(document).ready(function() {
 
  //create a moment object of current day and time. 
  let time = moment();
- // convert the moment object to show in string, target the node and update to current day and time.
+ // convert the moment object to show in string, target the node and update to current day
  $("#currentDay").text(time.format("dddd MMMM Do YYYY"));
 
 //On click save data to local storage to persist. 
 $("button").on("click", function(event){
     //value for the value of argument
-    var value = $(this).siblings("input").val().trim();
-​   // value for key for the argument
-    var key = $(this).parent().data("hour")
+    var todo = $(this).siblings("input").val().trim();
+​   // value for key of the argument
+    var hour = $(this).parent().data("hour");
 ​   //The setting to local storage of the Key value pair
-    localStorage.setItem(key, value)
+    localStorage.setItem(hour, todo);
 })
 
 //each loop for setting the local storage to the timeblocks and compare below
 $(".item").each(function(){
-    ​   //current hour from moment.js to compare
-        var hour = $(this).data("hour")
-        //pulling hour from moment.js to compare
-        var currentHour = time.hour()
+    ​   //This hour selected to compare
+        var hour = $(this).data("hour");
+        //pulling current hour from moment.js to compare
+        var currentHour = time.hour();
     ​   //pulling local storage hour 
-        var data = localStorage.getItem(hour)
-        $(this).children("input").val(data)
+        var data = localStorage.getItem(hour);
+        //pulling the value for the input and placing it there
+        $(this).children("input").val(data);
         ​//compare time block againts current time with Moment.js
         ​ //condition for past event update class to have BG of RED
             if(hour < currentHour){
-                $(this).addClass("past")
+                $(this).addClass("past");
             }
          //condition for present event update class to have BG of Green
    
             if(hour == currentHour){
-                $(this).addClass("present")
+                $(this).addClass("present");
             }
            //condition for present event update class to have BG of Green
  
             if(hour > currentHour){
-                $(this).addClass("future")
+                $(this).addClass("future");
             }
         })
         ​
